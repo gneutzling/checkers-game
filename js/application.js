ab21a0fcf9c013df80ad143e51fd30cb67ce157b
Gabriel Neutzling <gabrielneutzling@gmail.com>
2014-05-11 02:46:18 -0300

    refactoring and basic validations.

--- js/application.js
+++ js/application.js
@@ -18,7 +18,19 @@
     var app = {}, settings = {
         scope: "game-stage",
         board: "board",
-        score: "score"
+        score: {
+            element: "score",
+            playerOne: "score__player-one",
+            playerTwo: "score__player-two"
+        },
+        player: {
+            one: {
+                name: "player One"
+            },
+            two: {
+                name: "player Two"
+            }
+        }
     };
     /**
 	 * Init is like a summary of process.
@@ -27,21 +39,15 @@
         app.setup();
         app.build();
         app.attach();
+        app.updatePlayer();
+        app.updateScore();
         app.bind();
     };
     /**
 	 * Get elements from DOM and initialize some objects.
 	 */
     app.setup = function() {
-        app.scope = document.getElementById(settings.scope), app.board = app.scope.getElementsByClassName(settings.board)[0], 
-        app.score = app.scope.getElementsByClassName(settings.score)[0];
-        app.squares = null;
-        app.currentPiece = null;
-        app.currentSquare = null;
-        app.points = {
-            playerOne: 0,
-            playerTwo: 0
-        };
+        app.scope = document.getElementById(settings.scope), app.board = app.scope.getElementsByClassName(settings.board)[0];
     };
     /**
 	 * Create HTML for board and pieces.
@@ -54,10 +60,10 @@
                 var squareType = (y + x) % 2 ? "dark" : "light";
                 html.push('<span class="board__square ' + squareType + '" data-position="' + y + "-" + x + '">');
                 if (squareType === "dark" && y > 4) {
-                    html.push('<span class="board__piece player-one" data-piece-type="player-one">' + y + "-" + x + "</span>");
+                    html.push('<span class="board__piece player-one" data-piece-type="player-one"></span>');
                 }
                 if (squareType === "dark" && y < 3) {
-                    html.push('<span class="board__piece player-two" data-piece-type="player-two">' + y + "-" + x + "</span>");
+                    html.push('<span class="board__piece player-two" data-piece-type="player-two"></span>');
                 }
                 html.push("</span>");
             }
@@ -70,6 +76,44 @@
 	 */
     app.attach = function() {
         app.squares = document.getElementsByClassName("board__square dark");
+        app.score = app.scope.getElementsByClassName(settings.score.element)[0];
+        var elmPlayerOne = app.score.getElementsByClassName(settings.score.playerOne)[0].getElementsByTagName("span"), elmPlayerTwo = app.score.getElementsByClassName(settings.score.playerTwo)[0].getElementsByTagName("span");
+        app.selectedPiece = null;
+        app.selectedSquare = null;
+        app.selectedDestiny = null;
+        app.position = {
+            piece: {
+                x: null,
+                y: null
+            },
+            destiny: {
+                x: null,
+                y: null
+            },
+            diff: {
+                x: null,
+                y: null
+            }
+        };
+        app.player = {
+            one: {
+                name: settings.player.one.name,
+                score: 0,
+                dom: {
+                    name: elmPlayerOne[0],
+                    score: elmPlayerOne[1]
+                }
+            },
+            two: {
+                name: settings.player.two.name,
+                score: 0,
+                dom: {
+                    name: elmPlayerTwo[0],
+                    score: elmPlayerTwo[1]
+                }
+            }
+        };
+        app.isPlayerOne = true;
     };
     /**
 	 * General listeners.
@@ -89,8 +133,8 @@
     app.getPosition = function(element) {
         var dataPos = element.getAttribute("data-position").split("-");
         return {
-            x: dataPos[0],
-            y: dataPos[1]
+            y: parseInt(dataPos[0]),
+            x: parseInt(dataPos[1])
         };
     };
     /**
@@ -102,85 +146,175 @@
         return element.getAttribute("data-piece-type");
     };
     /**
-	 * @TODO: refactoring is needed.
+	 * @todo: Exibir um feedback para o usuário.
 	 */
-    app.move = function(target) {
-        var prevPiece = null, prevSquare = null, position = {
-            prev: {
-                x: null,
-                y: null
-            },
-            curr: {
-                x: null,
-                y: null
-            },
-            diff: {
-                x: null,
-                y: null
+    app.showMessage = function(message) {
+        console.log("### ", message);
+    };
+    /**
+	 * Update the visual score.
+	 */
+    app.updateScore = function() {
+        app.player.one.dom.score.innerHTML = app.player.one.score;
+        app.player.two.dom.score.innerHTML = app.player.two.score;
+    };
+    /**
+	 * Update player's name.
+	 */
+    app.updatePlayer = function() {
+        app.player.one.dom.name.innerHTML = app.player.one.name + ": ";
+        app.player.two.dom.name.innerHTML = app.player.two.name + ": ";
+    };
+    /**
+	 * Select a piece to the match.
+	 * @param  {object} target The selected square.
+	 */
+    app.choosePiece = function(target) {
+        if (app.hasPiece(target)) {
+            app.selectedPiece = target.getElementsByClassName("board__piece")[0];
+            var pieceType = app.getPieceType(app.selectedPiece);
+            if (app.isPlayerOne && pieceType === "player-one" || !app.isPlayerOne && pieceType === "player-two") {
+                app.selectedSquare = target;
+                app.selectedPiece.classList.add("selected");
+            } else {
+                app.selectedPiece = null;
+                app.showMessage("Espere a sua vez de jogar || você não pode jogar com uma peça do adversário.");
             }
-        };
-        if (app.currentPiece != null) {
-            // console.log('set position')
-            position.prev.y = parseInt(app.currentSquare.getAttribute("data-position").split("-")[0]);
-            position.prev.x = parseInt(app.currentSquare.getAttribute("data-position").split("-")[1]);
-            position.curr.y = parseInt(target.getAttribute("data-position").split("-")[0]);
-            position.curr.x = parseInt(target.getAttribute("data-position").split("-")[1]);
-            position.diff.y = position.prev.y - position.curr.y < 0 ? (position.prev.y - position.curr.y) * -1 : position.prev.y - position.curr.y;
-            position.diff.x = position.prev.x - position.curr.x < 0 ? (position.prev.x - position.curr.x) * -1 : position.prev.x - position.curr.x;
+        } else {
+            app.showMessage("Você deve escolher uma peça antes do destino.");
         }
-        if (app.currentSquare != null) {
-            app.currentSquare.classList.remove("selected");
-        }
-        prevPiece = app.currentPiece;
-        prevSquare = app.currentSquare;
-        app.currentSquare = target;
-        app.currentPiece = target.getElementsByClassName("board__piece")[0];
-        app.currentSquare.classList.add("selected");
-        // ve se n tem nenhuma peça na casa, se tá em branco.
-        if (app.currentPiece == null) {
-            // console.log('casa disponível.');
-            if (prevPiece != null) {
-                // console.log('tem uma peça selecionada.');
-                var pieceType = app.getPieceType(prevPiece);
-                if (pieceType === "player-one" && position.curr.y < position.prev.y || pieceType === "player-two" && position.curr.y > position.prev.y) {
-                    if (position.diff.y === 1 && position.diff.x === 1) {
-                        // console.log('andou para frente.');
-                        app.currentSquare.appendChild(prevPiece);
+    };
+    /**
+	 * Select a destiny to the piece.
+	 * @param  {object} target The selected square.
+	 */
+    app.chooseDestiny = function(target) {
+        app.selectedDestiny = target;
+    };
+    /**
+	 * Change the player's turn.
+	 */
+    app.changePlayer = function() {
+        app.isPlayerOne = !app.isPlayerOne;
+    };
+    /**
+	 * Configure the coordinates.
+	 */
+    app.setMatchPosition = function() {
+        app.position.piece.y = app.getPosition(app.selectedSquare).y;
+        app.position.piece.x = app.getPosition(app.selectedSquare).x;
+        app.position.destiny.y = app.getPosition(app.selectedDestiny).y;
+        app.position.destiny.x = app.getPosition(app.selectedDestiny).x;
+        app.position.diff.y = app.position.piece.y - app.position.destiny.y < 0 ? (app.position.piece.y - app.position.destiny.y) * -1 : app.position.piece.y - app.position.destiny.y;
+        app.position.diff.x = app.position.piece.x - app.position.destiny.x < 0 ? (app.position.piece.x - app.position.destiny.x) * -1 : app.position.piece.x - app.position.destiny.x;
+    };
+    /**
+	 * Check if there is piece in the square.
+	 * @param  {object}  square DOM element
+	 * @return {Boolean}        true if there is piece or false if not.
+	 */
+    app.hasPiece = function(square) {
+        var piece = square.getElementsByClassName("board__piece")[0];
+        return piece ? true : false;
+    };
+    /**
+	 * Check if is a valid movement.
+	 * @return {boolean} True for valid movement or false for not.
+	 */
+    app.validateMove = function() {
+        return app.isPlayerOne && app.position.destiny.y < app.position.piece.y || !app.isPlayerOne && app.position.destiny.y > app.position.piece.y ? true : false;
+    };
+    /**
+	 * The main rules of the game
+	 * 1 x 1 = a simple movement.
+	 * 2 x 2 = a movement with capture.
+	 */
+    app.checkMove = function() {
+        if (app.validateMove()) {
+            if (app.position.diff.x === 1 && app.position.diff.y === 1) {
+                app.movePiece();
+                app.changePlayer();
+            } else {
+                if (app.position.diff.x === 2 && app.position.diff.y === 2) {
+                    var capturedPiece = app.getCaptured();
+                    if (app.isPlayerOne) {
+                        if (capturedPiece && app.getPieceType(capturedPiece) === "player-two") {
+                            app.movePiece(capturedPiece);
+                            app.player.one.score++;
+                            app.updateScore();
+                        }
                     } else {
-                        if (position.diff.y === 2 && position.diff.x === 2) {
-                            var capturedPiece = null, capturedSquare = null, dataValue = null;
-                            if (pieceType === "player-one") {
-                                if (position.curr.x > position.prev.x) {
-                                    dataValue = position.curr.y + 1 + "-" + (position.curr.x - 1);
-                                } else {
-                                    dataValue = position.curr.y + 1 + "-" + (position.curr.x + 1);
-                                }
-                                capturedSquare = app.board.querySelectorAll('[data-position="' + dataValue + '"]')[0];
-                                capturedPiece = capturedSquare.getElementsByClassName("board__piece")[0];
-                                if (capturedPiece && app.getPieceType(capturedPiece) === "player-two") {
-                                    capturedPiece.remove();
-                                    app.currentSquare.appendChild(prevPiece);
-                                    app.points.playerOne++;
-                                }
-                            } else {
-                                if (position.curr.x > position.prev.x) {
-                                    dataValue = position.curr.y - 1 + "-" + (position.curr.x - 1);
-                                } else {
-                                    dataValue = position.curr.y - 1 + "-" + (position.curr.x + 1);
-                                }
-                                capturedSquare = app.board.querySelectorAll('[data-position="' + dataValue + '"]')[0];
-                                capturedPiece = capturedSquare.getElementsByClassName("board__piece")[0];
-                                if (capturedPiece && app.getPieceType(capturedPiece) === "player-one") {
-                                    capturedPiece.remove();
-                                    app.currentSquare.appendChild(prevPiece);
-                                    app.points.playerTwo++;
-                                }
-                            }
+                        if (capturedPiece && app.getPieceType(capturedPiece) === "player-one") {
+                            app.movePiece(capturedPiece);
+                            app.player.two.score++;
+                            app.updateScore();
                         }
                     }
                 }
             }
+        } else {
+            app.showMessage("Você deve movimentar a peça para frente.");
         }
+    };
+    /**
+	 * Move the piece to the destiny. If has capture, remove.
+	 * @param  {object} captured DOM element to remove or false.
+	 */
+    app.movePiece = function(captured) {
+        app.selectedDestiny.appendChild(app.selectedPiece);
+        if (captured) {
+            captured.remove();
+        }
+    };
+    /**
+	 * Get the captured piece.
+	 * @return {object} DOM element or false.
+	 */
+    app.getCaptured = function() {
+        var dataValue = null, captured = null;
+        if (app.isPlayerOne) {
+            if (app.position.piece.x > app.position.destiny.x) {
+                dataValue = app.position.destiny.y + 1 + "-" + (app.position.destiny.x + 1);
+            } else {
+                dataValue = app.position.destiny.y + 1 + "-" + (app.position.destiny.x - 1);
+            }
+        } else {
+            if (app.position.piece.x > app.position.destiny.x) {
+                dataValue = app.position.destiny.y - 1 + "-" + (app.position.destiny.x + 1);
+            } else {
+                dataValue = app.position.destiny.y - 1 + "-" + (app.position.destiny.x - 1);
+            }
+        }
+        captured = app.board.querySelectorAll('[data-position="' + dataValue + '"]')[0];
+        return captured.getElementsByClassName("board__piece")[0] || false;
+    };
+    /**
+	 * Setup the process of current match.
+	 * @param  {object} target DOM element clicked.
+	 */
+    app.move = function(target) {
+        if (app.selectedPiece == null) {
+            app.choosePiece(target);
+        } else {
+            if (!app.hasPiece(target)) {
+                app.chooseDestiny(target);
+                app.setMatchPosition();
+                app.checkMove();
+                app.resetVars();
+            } else {
+                app.showMessage("Você deve escolher um destino válido.");
+                app.resetVars();
+            }
+        }
+    };
+    /**
+	 * Reset global variables.
+	 */
+    app.resetVars = function() {
+        app.selectedPiece.classList.remove("selected");
+        app.selectedPiece = null;
+        app.selectedSquare = null;
+        app.selectedDestiny = null;
     };
     /**
 	 * Let's go =]
